f982cf88915c2d89989da072da45a918f6fce28e
vishaul <85075314+vishaul@users.noreply.github.com>
2021-06-01 14:22:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    
+    var P = document.getElementById("principle").value; 
+
+    if(P<=0){
+        alert("Enter a positive number");
+        P.focus();
+        return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var noofyears=document.getElementById("years").value;
+    
+    const p=Number(P);
+    const r=Number(rate);
+    const n=Number(noofyears);
+    
+    var amount=parseFloat((p*r*n)/100);
+    
+    var year = new Date().getFullYear() + n;
+    document.getElementById('result').innerHTML="if you deposit"+p+",\<br\>at an interest rate of "+r+"%\<br\>you will receive an amount of "+amount+"\<br\>in the year "+year+"\<br\>";
+
+    
     
 }
+
+function updateRate() //update the rate
+{
+    var rateval=document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+
+
+}
+
+
+
+
+    
         
